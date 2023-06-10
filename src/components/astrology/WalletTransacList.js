c26cfeff98f9c612fee441315a532a4a9206072a
prandeveloper <pranaydevelopersveltosest@gmail.com>
2023-06-10 17:53:02 +0530

    today 10/06/23

--- src/components/astrology/WalletTransacList.js
+++ src/components/astrology/WalletTransacList.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { Table } from "reactstrap";
 import "../../assets/scss/astroteam.scss";
 import axiosConfig from "../../axiosConfig";
 import { Container, Row, Col } from "reactstrap";
 import LayoutOne from "../../layouts/LayoutOne";
 import astrologinbg from "../../assets/img/astrologin-bg.jpg";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-balham.css";
 
 class WalletTransacList extends React.Component {
   constructor(props) {
@@ -13,13 +15,54 @@ class WalletTransacList extends React.Component {
       WalletTransacList: [],
       data: {},
     };
+    this.state = {
+      TablerowData: [],
+      columns: [
+        {
+          headerName: "ID",
+          field: "transaction_id",
+          sortable: true,
+          filter: true,
+        },
+        {
+          headerName: "Amount Before",
+          field: "beforeAmt",
+          sortable: true,
+          filter: true,
+        },
+        {
+          headerName: "Transaction Amount",
+          field: "amount",
+          sortable: true,
+          filter: "agNumberColumnFilter",
+        },
+        {
+          headerName: "Amount After",
+          field: "finalAmt",
+          sortable: true,
+          filter: "agNumberColumnFilter",
+        },
+        {
+          headerName: "Date/Time",
+          field: "createdAt",
+          sortable: true,
+          filter: "agNumberColumnFilter",
+        },
+        {
+          headerName: "	Transaction Type",
+          field: "tran_Type",
+          sortable: true,
+          filter: "agNumberColumnFilter",
+        },
+      ],
+    };
   }
   componentDidMount = () => {
     let userId = JSON.parse(localStorage.getItem("user_id"));
     axiosConfig
       .get(`/user/walletHistory/${userId}`)
       .then(response => {
-        console.log(response.data);
+        console.log(response.data.data);
         if (response.data.status === true) {
           this.setState({
             WalletTransacList: response.data.data,
@@ -28,7 +71,6 @@ class WalletTransacList extends React.Component {
       })
       .catch(error => {
         console.log(error);
-        console.log(error.response);
       });
     axiosConfig
       .get(`/user/viewoneuser/${userId}`)
@@ -42,6 +84,7 @@ class WalletTransacList extends React.Component {
         console.log(error);
       });
   };
+
   render() {
     const { WalletTransacList } = this.state;
     return (
@@ -62,7 +105,6 @@ class WalletTransacList extends React.Component {
               backgroundPosition: "center center",
               backgroundRepeat: " no-repeat",
               textAlign: "center",
-              // backgroundColor: "#ffcc01", width: "100%", padding: "70px 0px", backgroundSize: "cover"
             }}
           >
             <Container>
@@ -81,96 +123,25 @@ class WalletTransacList extends React.Component {
           </div>
         </section>
         <section>
-          <Container>
-            <Row>
-              <Col lg="12">
-                <div className="">
-                  <Table striped className="">
-                    <thead>
-                      <tr>
-                        <th># T/C ID.</th>
-                        <th>Amount Before</th>
-                        <th>Transaction Amount</th>
-                        <th>Amount After</th>
-                        <th>Date/Time</th>
-                        <th>Transaction Type</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {WalletTransacList.length
-                        ? WalletTransacList.map((WalletTransac, index) => {
-                            return (
-                              <tr>
-                                <th>
-                                  {WalletTransac.transaction_id}
-                                  {WalletTransac.conversationId}
-                                </th>
-                                <td>{WalletTransac.beforeAmt}</td>
-                                <td>
-                                  {WalletTransac.deductedAmt}{" "}
-                                  {WalletTransac.creditedAmt}
-                                </td>
-                                <td>{WalletTransac.finalAmt}</td>
-                                <td>{WalletTransac.createdAt}</td>
-                                <td>{WalletTransac.tran_Type}</td>
-                              </tr>
-                            );
-                          })
-                        : null}
-                      {/* <tr>
-                                                <th scope="row">2</th>
-                                                <td>Chat With Monish For 2 Minutes</td>
-                                                <td>doc</td>
-                                                <td>30.00</td>
-                                                <td>sept 05, 2022 11:32AM</td>
-                                                <td>
-                                                    <Link className="Tansdel">
-                                                        <i class="fa fa-trash-o" aria-hidden="true"></i>
-                                                    </Link>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">3</th>
-                                                <td>Chat With Monish For 2 Minutes</td>
-                                                <td>doc</td>
-                                                <td>30.00</td>
-                                                <td>sept 05, 2022 11:32AM</td>
-                                                <td>
-                                                    <Link className="Tansdel">
-                                                        <i class="fa fa-trash-o" aria-hidden="true"></i>
-                                                    </Link>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">4</th>
-                                                <td>Chat With Monish For 2 Minutes</td>
-                                                <td>doc</td>
-                                                <td>30.00</td>
-                                                <td>sept 05, 2022 11:32AM</td>
-                                                <td>
-                                                    <Link className="Tansdel">
-                                                        <i class="fa fa-trash-o" aria-hidden="true"></i>
-                                                    </Link>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">5</th>
-                                                <td>Chat With Monish For 2 Minutes</td>
-                                                <td>doc</td>
-                                                <td>30.00</td>
-                                                <td>sept 05, 2022 11:32AM</td>
-                                                <td>
-                                                    <Link className="Tansdel">
-                                                        <i class="fa fa-trash-o" aria-hidden="true"></i>
-                                                    </Link>
-                                                </td>
-                                            </tr> */}
-                    </tbody>
-                  </Table>
-                </div>
-              </Col>
-            </Row>
-          </Container>
+          {/* <Container>
+            <Row > */}
+
+          <div
+            className="ag-theme-balham"
+            style={{
+              height: "80vh",
+              width: "100%",
+            }}
+          >
+            <AgGridReact
+              columnDefs={this.state?.columns}
+              rowData={WalletTransacList}
+              pagination={true}
+              paginationPageSize={50}
+            />
+          </div>
+          {/* </Row>
+          </Container> */}
         </section>
       </LayoutOne>
     );
