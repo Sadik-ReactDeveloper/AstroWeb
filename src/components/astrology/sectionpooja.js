a3226bdbde3e542369eec747f71713e8774d4f2e
prandeveloper <pranaydevelopersveltosest@gmail.com>
2023-07-08 18:46:50 +0530

    Launch book pooja

--- src/components/astrology/sectionpooja.js
+++ src/components/astrology/sectionpooja.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { Container, Row, Col } from "reactstrap";
 import textbottom from "../../assets/img/textbottom.png";
 import axiosConfig from "../../axiosConfig";
+import Homebookpooja from "./Homebookpooja.js";
 import Swiper from "react-id-swiper";
 class SectionPooja extends React.Component {
   state = {
@@ -16,45 +17,55 @@ class SectionPooja extends React.Component {
   render() {
     const { productList } = this.state;
     return (
-      <Container>
-        <div className="heading mt-40">
-          <h2>Our Latest Products</h2>
-          <img src={textbottom} alt="" />
-        </div>
-        <Row>
-          <div className="col-lg-12 col-md-12 mb-30">
-            <div className="category-home">
-              <section className="pt-0">
-                <div className="container">
-                  <Row>
-                    {this.state.productList.length
-                      ? productList.slice(0, 4).map((product, index) => {
-                          return (
-                            <Col md="3">
-                              <div className="product-grid8">
-                                <div class="product-image8">
-                                  <Link to="/poojadetail">
-                                    <img
-                                      src={product.image}
-                                      alt=""
-                                      className="Ptd-img"
-                                    />
-                                  </Link>
-                                </div>
-                                <div className="product-content">
-                                  <div className="price d-flex">
-                                    <div>
-                                      <i
-                                        class="fa fa-inr"
-                                        aria-hidden="true"
-                                      ></i>
-                                    </div>
-                                    <div className=" px-2">{product.price}</div>
+      <>
+        <Container>
+          <div className="heading mt-40">
+            <h2>Newly Launched</h2>
+            <img src={textbottom} alt="" />
+          </div>
+          <Homebookpooja />
+        </Container>
+        <Container>
+          <div className="heading mt-40">
+            <h2>Our Latest Products</h2>
+            <img src={textbottom} alt="" />
+          </div>
+          <Row>
+            <div className="col-lg-12 col-md-12 mb-30">
+              <div className="category-home">
+                <section className="pt-0">
+                  <div className="container">
+                    <Row>
+                      {this.state.productList.length
+                        ? productList.slice(0, 4).map((product, index) => {
+                            return (
+                              <Col md="3">
+                                <div className="product-grid8">
+                                  <div class="product-image8">
+                                    <Link to="/poojadetail">
+                                      <img
+                                        src={product.image}
+                                        alt=""
+                                        className="Ptd-img"
+                                      />
+                                    </Link>
                                   </div>
-                                  <h3 className="title">
-                                    <Link>
-                                      {product.productname.substr(0, 9)}
-                                      {/* {product.productname.length > 24 ? (
+                                  <div className="product-content">
+                                    <div className="price d-flex">
+                                      <div>
+                                        <i
+                                          class="fa fa-inr"
+                                          aria-hidden="true"
+                                        ></i>
+                                      </div>
+                                      <div className=" px-2">
+                                        {product.price}
+                                      </div>
+                                    </div>
+                                    <h3 className="title">
+                                      <Link>
+                                        {product.productname.substr(0, 9)}
+                                        {/* {product.productname.length > 24 ? (
                                         <span className="">
                                           {product.productname.substr(0, 9)}
                                           ...See more
@@ -62,25 +73,26 @@ class SectionPooja extends React.Component {
                                       ) : (
                                         product.productname
                                       )} */}
+                                      </Link>
+                                    </h3>
+                                    <Link className="all-deals">
+                                      View More
+                                      <i class="fa fa-angle-right icon"></i>
                                     </Link>
-                                  </h3>
-                                  <Link className="all-deals">
-                                    View More
-                                    <i class="fa fa-angle-right icon"></i>
-                                  </Link>
+                                  </div>
                                 </div>
-                              </div>
-                            </Col>
-                          );
-                        })
-                      : null}
-                  </Row>
-                </div>
-              </section>
+                              </Col>
+                            );
+                          })
+                        : null}
+                    </Row>
+                  </div>
+                </section>
+              </div>
             </div>
-          </div>
-        </Row>
-      </Container>
+          </Row>
+        </Container>
+      </>
     );
   }
 }
