5f25dfc3aa4ce808cb6c7a1b67b37643b44b56ec
prandeveloper <pranaydevelopersveltosest@gmail.com>
2023-08-02 14:16:34 +0530

    video deduction

--- src/components/astrology/zegocloud/VideoCall.js
+++ src/components/astrology/zegocloud/VideoCall.js
@@ -41,18 +41,19 @@ export default function App() {
     handlestartinterval();
   };
   const handlestartinterval = () => {
-    this.apicall.current = setInterval(() => {
+    ref.current = setInterval(() => {
       let userId = JSON.parse(localStorage.getItem("user_id"));
-      let astroId = localStorage.getItem("videoCallAstro_id");
-      sessionStorage.setItem("typeofcall", "Chat");
+      let astroId = localStorage.getItem("astroId");
+      sessionStorage.setItem("typeofcall", "Video");
 
       let payload = {
         userId: userId,
         astroId: astroId,
-        status: true,
+        // status: true,
+        type: "Video",
       };
       axiosConfig
-        .post(`/user/addCallDuration`, payload)
+        .post(`/user/deductChatBalance`, payload)
         .then(res => {
           console.log("callduration", res.data);
           Fetchuserdetail();
@@ -85,7 +86,8 @@ export default function App() {
   let myMeeting = async element => {
     // generate Kit Token
     const appID = 1011009319;
-    const response = getUserID() || randomID(5).toString();
+    const name = JSON.parse(localStorage.getItem("userData"));
+    const response = name.Userinfo?.full_name || randomID(5).toString();
     const serverSecret = "0b7c5477f07346c1a906c99d3f171f2e";
     const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
       appID,
@@ -98,6 +100,7 @@ export default function App() {
     // Create instance object from Kit Token.
     const zp = ZegoUIKitPrebuilt.create(kitToken);
     let userID = JSON.parse(localStorage.getItem("user_id"));
+
     // start the call
 
     zp.joinRoom({
@@ -117,28 +120,48 @@ export default function App() {
         },
       ],
       scenario: {
-        mode: ZegoUIKitPrebuilt.VideoConference,
+        mode: ZegoUIKitPrebuilt.OneONoneCall,
       },
       preJoinViewConfig: {
         title: "Join Astrologer Video Call",
       },
 
-      onUserJoin: async data => {
-        handleStart();
+      onJoinRoom: () => {
+        let obj = {
+          astroid: localStorage.getItem("astroId"),
+          videoLink: roomID,
+          userid: userID,
+        };
+        axiosConfig
+          .post("/user/send_VideoLink", obj)
+          .then(response => {
+            // console.log(response.data.data);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      },
+      onLeaveRoom: async () => {
+        handleStop();
         let astroid = localStorage.getItem("astroId");
-        let load = {
+
+        let value = {
           userId: userID,
           astroId: astroid,
-          type: "Video",
         };
-        await axiosConfig
-          .post(`/admin/notificationList`, load)
+        axiosConfig
+          .post(`/user/changeStatus`, value)
           .then(res => {
-            console.log("Video Calling", res.data);
+            console.log("CloneJob Stop", res);
+            localStorage.removeItem("CurrentChat_userid");
+            history.push("/astrorating");
           })
           .catch(err => {
             console.log(err.response.data);
           });
+      },
+      onUserJoin: () => {
+        handleStart();
       },
       onUserLeave: async () => {
         handleStop();
@@ -151,7 +174,7 @@ export default function App() {
         axiosConfig
           .post(`/user/changeStatus`, value)
           .then(res => {
-            console.log(res);
+            console.log("CloneJob Stop", res);
             localStorage.removeItem("CurrentChat_userid");
             history.push("/astrorating");
           })
@@ -159,21 +182,7 @@ export default function App() {
             console.log(err.response.data);
           });
       },
-      onJoinRoom: () => {
-        let obj = {
-          astroid: localStorage.getItem("astroId"),
-          videoLink: roomID,
-          userid: userID,
-        };
-        axiosConfig
-          .post("user/send_VideoLink", obj)
-          .then(response => {
-            // console.log(response.data.data.createdAt);
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      },
+
       showScreenSharingButton: false,
     });
   };
